d973b6e2c910d85d5074aa6dda54ef9e523f33fc
Farevell <74495058+Farevell@users.noreply.github.com>
2024-03-22 12:40:56 +0800

    Update __init__.py

--- src/plugins/DicePP/module/misc/__init__.py
+++ src/plugins/DicePP/module/misc/__init__.py
@@ -1,5 +1,4 @@
 from .jrrp_command import JrrpCommand
-from .log_command import LogCommand
 from .dnd_command import UtilsDNDCommand
 from .coc_command import UtilsCOCCommand
 from .statistics_cmd import StatisticsCommand
